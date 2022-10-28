68f7cac2aaf5408e57cea49375028ce9fbe0055d
Periphanes <63851663+Periphanes@users.noreply.github.com>
2022-10-29 01:29:04 +0900

    LSTM Update

--- models/LSTM.py
+++ models/LSTM.py
@@ -11,13 +11,18 @@ class LSTM(torch.nn.Module):
         self.batch_size = args.batch_size
         self.input_size = 15
         self.hidden_size = 16
-        self.num_layers = 1
+        self.num_layers = 2
 
         self.lstm = nn.LSTM(input_size = self.input_size, hidden_size = self.hidden_size, \
                             num_layers = self.num_layers, batch_first = True)
         
         self.relu = nn.ReLU()
         self.regresser = nn.Linear(self.hidden_size, 1)
+
+        self.regresser_list = nn.ModuleList()
+
+        for _ in range(args.prediction_years):
+            self.regresser_list.append(nn.Linear(self.hidden_size, 1))
     
     def forward(self, x):
         output, (hn, cn) = self.lstm(x)
@@ -25,5 +30,13 @@ class LSTM(torch.nn.Module):
         output = output.squeeze()
         output = output[-1,:]
 
-        regress = self.regresser(output)
-        return regress
+        regress_list = []
+        for idx, regress in enumerate(self.regresser_list):
+            regress_list.append(regress(output))
+
+        output = torch.stack(regress_list).squeeze().unsqueeze(0)
+
+        if self.args.prediction_years == 1:
+            output = output.unsqueeze(0)
+
+        return output
