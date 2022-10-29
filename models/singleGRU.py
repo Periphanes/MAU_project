7857c021e40c842428934bf1adbdce9fa3045925
Periphanes <63851663+Periphanes@users.noreply.github.com>
2022-10-29 17:34:30 +0900

    Single Year Prediction Versions for XAI Analysis

---
+++ models/singleGRU.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+
+class GRU(torch.nn.Module):
+    def __init__(self, args):
+        super(GRU, self).__init__()
+        self.args = args
+        self.batch_size = args.batch_size
+        self.input_size = 15
+        self.hidden_size = 16
+        self.num_layers = 1
+
+        self.lstm = nn.GRU(input_size = self.input_size, hidden_size = self.hidden_size, \
+                            num_layers = self.num_layers, batch_first = True)
+        
+        self.relu = nn.ReLU()
+        self.regresser = nn.Linear(self.hidden_size, 1)
+
+        self.regresser_list = nn.ModuleList()
+
+        for _ in range(args.prediction_years):
+            self.regresser_list.append(nn.Linear(self.hidden_size, 1))
+    
+    def forward(self, x):
+        output, hn = self.lstm(x)
+        
+        output = output.squeeze()
+        output = output[-1,:]
+
+        output = self.regresser(output)
+
+        return output
---
+++ models/singleTrans.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+import math
+from typing import Tuple
+
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            x: Tensor, shape [seq_len, batch_size, embedding_dim]
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
+class TransformerModel(torch.nn.Module):
+    def __init__(self, args):
+        super(TransformerModel, self).__init__()
+        self.args = args
+        self.batch_size = args.batch_size
+        self.num_heads = args.trans_head
+        self.dropout = args.trans_dropout
+        self.num_layers = args.trans_layer
+        self.model_dim = args.trans_dim
+        self.input_dim = 1882
+
+        self.pos_encoder = PositionalEncoding(self.model_dim, self.dropout)
+        self.encoder_layers = TransformerEncoderLayer(self.model_dim, self.num_heads, self.model_dim, self.dropout)
+        self.trasnformer_encoder = TransformerEncoder(self.encoder_layers, self.num_layers)
+
+        self.cls_token = torch.rand((1,1,self.model_dim))
+
+        self.init_fc = nn.Linear(self.input_dim, self.model_dim)
+
+        # self.lstm = nn.LSTM(input_size = self.input_size, hidden_size = self.hidden_size, \
+        #                     num_layers = self.num_layers, batch_first = True)
+        
+        self.relu = nn.ReLU()
+        self.regresser = nn.Linear(self.model_dim, 1)
+
+        self.regresser_list = nn.ModuleList()
+
+        for _ in range(args.prediction_years):
+            self.regresser_list.append(nn.Linear(self.model_dim, 1))
+
+    def forward(self, x):
+        # print(x.shape)
+        embedding = []
+        for i in range(len(x[0][0])):
+            embedding.append(self.init_fc(x[:,:,i]))
+        # embedding = self.init_fc(x)
+        embedding = torch.cat(embedding)
+        #embedding = embedding.swapaxes(0,1)
+
+        embedding = embedding.unsqueeze(0)
+        # print(embedding.shape)
+        input_pos = self.pos_encoder(embedding)
+        # print(self.cls_token.shape)
+        final_inp = torch.cat((self.cls_token, input_pos), axis=1)
+        # print(final_inp.shape)
+
+        output = self.trasnformer_encoder(final_inp)
+
+        cls_output = output[:,0,:]
+
+        # test_out = self.regresser(cls_output)
+        # print(test_out.shape)
+
+        output = self.regresser(cls_output)
+
+        # print(regress.shape)
+        return output
